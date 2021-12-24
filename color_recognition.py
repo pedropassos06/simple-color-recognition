402b77e0bffbab790cea18e777ed5c6571a9a082
Pedro Passos <pedropassos.yt@gmail.com>
2021-12-24 17:25:01 -0500

    done for red color

--- color_recognition.py
+++ color_recognition.py
@@ -4,14 +4,30 @@ import numpy as np
 class ColorRecognition:
 
     def __init__(self):
-        #blue HSV range
-        self.low_blue = np.array([40, 85, 0])
-        self.high_blue = np.array([121, 255, 255])
+
+        #red HSV range
+        self.low_red = np.array([150, 81, 0])
+        self.high_red = np.array([180, 255, 255])
+
+        #camera ID
+        self.cam_ID = 0
+
+        #kernel size
+        self.kernel_size = (5,5)
+
+        #thickness of contours
+        self.cont_thickness = 3
+
+        #colors
+        self.green = (0, 255, 0)
+
+        #min area
+        self.min_area = 1000
     
     def detect(self):
 
         #start using camera device
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(self.cam_ID)
         
         while True:
 
@@ -23,25 +39,25 @@ class ColorRecognition:
                 break
 
             #blur the frame to avoid noise
-            blur = cv2.GaussianBlur(frame, (5,5), 0)
+            blur = cv2.GaussianBlur(frame, self.kernel_size, 0)
             
             #create mask
             hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-            mask = cv2.inRange(hsv, self.low_blue, self.high_blue)
+            mask = cv2.inRange(hsv, self.low_red, self.high_red)
 
             #find and draw contours
-            _, contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
             for contour in contours:
                 #ensure area is big enough
                 area = cv2.contourArea(contour)
 
-                if area > 60:
-                    cv2.drawControus(frame, contour, -1, (0, 0, 255), 3)
+                if area > self.min_area:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(frame, (x,y), ( x+w, y+h ), self.green, self.cont_thickness)
 
             #display
             cv2.imshow("Frame", frame)
-            cv2.imshow("Mask", mask)
 
             #is esc is pressed, quit detection process
             key = cv2.waitKey(1)
@@ -54,5 +70,3 @@ class ColorRecognition:
 
 cr = ColorRecognition()
 cr.detect()
-
-
